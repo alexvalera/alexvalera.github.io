6d683c00c06f20753d7e97fc800b83f97c44e63c
Alexander Valera <alex.p.valera@gmail.com>
2015-06-07 23:02:00 -0400

    New Design

---
+++ Javascript/main.js
@@ -0,0 +1,27 @@
+//
+//$('#bio-container').hide().fadeIn(1000);
+//$('#clean').hide().fadeIn(function()
+//{
+//$('#ampersand').fadeIn(1000)
+//}, 1000);
+
+/**
+Banner text Fade In Entrance
+*/
+$('#clean').hide();
+$('#ampersand').hide();
+$('#bold').hide();
+$('#links_title').hide();
+$('#portfolio_btn').hide();
+$('#clean').fadeIn(1000, function()
+{
+    $('#ampersand').hide().fadeIn(1000, function()
+    {
+        $('#bold').hide().fadeIn(1000, function()
+        {
+        
+            $('#links_title').hide().fadeIn(500);
+//            $('#portfolio_btn').hide().fadeIn(500);
+        });
+    });
+});
