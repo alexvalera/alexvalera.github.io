660037c8fd294d82b74e7ae858adf83eed244e23
Alexander Valera <alex.p.valera@gmail.com>
2015-07-14 22:40:09 -0400

    Changing top, adding media queries

--- Javascript/main.js
+++ Javascript/main.js
@@ -8,20 +8,22 @@
 /**
 Banner text Fade In Entrance
 */
-$('#clean').hide();
-$('#ampersand').hide();
-$('#bold').hide();
-$('#links_title').hide();
-$('#portfolio_btn').hide();
-$('#clean').fadeIn(1000, function()
-{
-    $('#ampersand').hide().fadeIn(1000, function()
-    {
-        $('#bold').hide().fadeIn(1000, function()
-        {
-        
-            $('#links_title').hide().fadeIn(500);
-//            $('#portfolio_btn').hide().fadeIn(500);
-        });
-    });
-});
+//$('#clean').hide();
+//$('#ampersand').hide();
+//$('#bold').hide();
+//$('#links_title').hide();
+//$('#portfolio_btn').hide();
+//$('#clean').fadeIn(1000, function()
+//{
+//    $('#ampersand').hide().fadeIn(1000, function()
+//    {
+//        $('#bold').hide().fadeIn(1000, function()
+//        {
+//        
+//            $('#links_title').hide().fadeIn(500);
+////            $('#portfolio_btn').hide().fadeIn(500);
+//        });
+//    });
+//});
+$('#bold').hide(); 
+$('#bold').fadeIn(1000);
